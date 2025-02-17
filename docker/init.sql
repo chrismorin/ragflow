3e742c0f20b06ff5c314bd14d5f6e8711f3869c3
chrismorin <chris@chrismorin.net>
2025-02-17 09:34:48 -0600

    Update init.sql

--- docker/init.sql
+++ docker/init.sql
@@ -1,2 +1,8 @@
 CREATE DATABASE IF NOT EXISTS rag_flow;
 USE rag_flow;
+
+-- Allow root to connect from any host
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'infini_rag_flow';
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'infini_rag_flow';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
